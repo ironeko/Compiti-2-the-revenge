98bf6561471200bf79a10e48d986e0ab903b57b7
Matteo <60699033+ironeko@users.noreply.github.com>
2020-11-06 15:27:59 +0100

    Add files via upload

---
+++ 2020_11_06_Triangle/src/Dot.java
@@ -0,0 +1,39 @@
+public class Dot {
+	//attributi
+	private double x;
+	private double y;
+	
+	//costruttore
+	public Dot(double x,double y) {
+		this.x=x;
+		this.y=y;
+		
+	}
+	
+	//get & set
+	public double getX() {
+		return x;
+	}
+	public double getY() {
+		return y;
+	}
+	public void setX(double x) {
+		this.x=x;
+	}
+	public void setY(double y) {
+		this.y=y;
+	}
+	
+	//distance
+	
+	public double distance(Dot p2) {
+		return Math.sqrt(Math.pow(p2.getX()-x,2 )+Math.pow(p2.getY()-y,2 ));
+	}
+	
+	//toString
+	public String toString() {
+		String app;
+		app="x: "+x+"\ty: "+y;
+		return app; //return "x: "+x+"\ty: "+y;
+	}
+}
