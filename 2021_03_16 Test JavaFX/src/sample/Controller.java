64710ce030745575e386783f2db50d9dc2288f7f
Matteo <60699033+ironeko@users.noreply.github.com>
2021-03-16 09:29:36 +0100

    FleXXXXXXXXXXXXXXXXX
    FleXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX

---
+++ 2021_03_16 Test JavaFX/src/sample/Controller.java
@@ -0,0 +1,15 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
+public class Controller {
+    public Button pinolo;
+    public Text gino;
+
+
+    public void sos(ActionEvent actionEvent) {
+        gino.setText("WE LOVE LUCHI <3");
+    }
+}
